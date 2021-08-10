7e50ecc1af7625cce9d408962927f73c39b9a2db
Thomas Vanelstlande <thomas_vanelstlande@hotmail.com>
2021-08-10 11:16:05 +0200

    project finished

--- philo/srcs/actions.c
+++ philo/srcs/actions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   actions.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tvanelst <tvanelst.student@19.be>          +#+  +:+       +#+        */
+/*   By: tvanelst <tvanelst@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 17:40:51 by tvanelst          #+#    #+#             */
-/*   Updated: 2021/08/09 16:38:12 by tvanelst         ###   ########.fr       */
+/*   Updated: 2021/08/10 09:56:58 by tvanelst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ void	take_forks_and_eat(t_philo *philo)
 	philo->last_meal = get_time_stamp(settings->start);
 	display_action(philo, EAT);
 	philo->number_of_meal++;
-	usleep(settings->time_to_eat * 750);
+	usleep(settings->time_to_eat * 800);
 	while (get_time_stamp(settings->start) - philo->last_meal
 		< settings->time_to_eat)
-		usleep(900);
+		usleep(50);
 }
 
 void	*eat_and_sleep(void *arg)
@@ -45,13 +45,14 @@ void	*eat_and_sleep(void *arg)
 		pthread_mutex_unlock(philo->fork_l);
 		pthread_mutex_unlock(philo->fork_r);
 		display_action(philo, SLEEP);
-		usleep(philo->settings->time_to_sleep * 750);
+		usleep(philo->settings->time_to_sleep * 800);
 		while (get_time_stamp(settings->start) - philo->last_meal
 			< settings->time_to_sleep + settings->time_to_eat)
-			usleep(900);
+			usleep(50);
 		if (!settings->max_set
 			|| philo->number_of_meal < settings->max_num_of_meal)
 			display_action(philo, THINK);
+		usleep(500);
 	}
 	return (NULL);
 }
